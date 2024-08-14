2eec3fe490d1c21f44b2997728b604932adb109d
Kimi Rettig <sarahkimirettig@gmail.com>
2024-08-14 15:52:52 -0400

    Commenting out gameMethods code
    Originally had queries in gameMethods file but kept getting invalid response error; api works with queries in gameRoutes file

--- services/gameMethods.js
+++ services/gameMethods.js
@@ -1,102 +1,110 @@
-var express = require("express");
-const helper = require("../helper");
-const connection = require("../config");
+// var express = require("express");
+// const helper = require("../helper");
+// const connection = require("../config");
 
-async function getGameCategories() {
-  try {
-    const rows = await connection.query(`SELECT * FROM categories`);
-    //helper only returns data if rows are not empty
-    const data = helper.emptyOrRows(rows[0].id);
-    console.log(data);
-    return data;
-  } catch (err) {
-    return err.stack;
-  }
-}
+// const test = () => {
+//   const test = connection.query(`SELECT * FROM categories`);
+//   return test;
+// };
 
-async function getCategoryClues(catid) {
-  try {
-    const rows = await connection.query(
-      `SELECT * FROM clues WHERE category_id = ${catid} ORDER BY ASC`
-    );
-    return rows;
-  } catch (err) {
-    return err.stack;
-  }
-}
+// async function getGameCategories() {
+// important note: doesn't work connection query needs to be in the routes file not in a separate methods file
+//better to organize routes into separate files and have query statements in the routes than to have methods separated from the routes?
+// try {
+//   return await connection.query(
+//     `SELECT * FROM categories`,
+//     function (error, results, fields) {
+//       res.json(results);
+//     }
+//   );
+// } catch (err) {
+//   return err.stack;
+// }
+// }
 
-async function getAllClues() {
-  try {
-    const rows = await connection.query(`SELECT * FROM clues`);
-    console.log("getAllClues  " + rows);
-    return rows;
-  } catch (err) {
-    return err.stack;
-  }
-}
+// async function getCategoryClues(catid) {
+//   try {
+//     const rows = await connection.query(
+//       `SELECT * FROM clues WHERE category_id = ${catid} ORDER BY ASC`
+//     );
+//     return rows;
+//   } catch (err) {
+//     return err.stack;
+//   }
+// }
 
-async function getGames() {
-  try {
-    const rows = await connection.query(`SELECT * FROM games`);
-    return rows;
-  } catch (err) {
-    return err.stack;
-  }
-}
+// async function getAllClues() {
+//   try {
+//     const rows = await connection.query(`SELECT * FROM clues`);
+//     console.log("getAllClues  " + rows);
+//     return rows;
+//   } catch (err) {
+//     return err.stack;
+//   }
+// }
 
-async function getClue(id) {
-  try {
-    const rows = await connection.query(
-      `SELECT * FROM clues WHERE clue_id = ${id}`
-    );
-    console.log("single clue getClue" + JSON.stringify(rows));
-    return rows;
-  } catch (err) {
-    return err.stack;
-  }
-}
+// async function getGames() {
+//   try {
+//     const rows = await connection.query(`SELECT * FROM games`);
+//     return rows;
+//   } catch (err) {
+//     return err.stack;
+//   }
+// }
 
-async function updateClue(id, answeredClue) {
-  try {
-    const rows = await connection.query(
-      `UPDATE clues SET answered = ${answeredClue} WHERE clue_id= ${id}`
-    );
-    console.log("single clue updateClue" + JSON.stringify(answeredClue));
-    return rows;
-  } catch (err) {
-    return err.stack;
-  }
-}
+// async function getClue(id) {
+//   try {
+//     const rows = await connection.query(
+//       `SELECT * FROM clues WHERE clue_id = ${id}`
+//     );
+//     console.log("single clue getClue" + JSON.stringify(rows));
+//     return rows;
+//   } catch (err) {
+//     return err.stack;
+//   }
+// }
 
-async function resetClues() {
-  try {
-    console.log("reset games");
-    const rows = await connection.query(`UPDATE clues SET answered = null
-    `);
-    return rows;
-  } catch (err) {
-    return err.stack;
-  }
-}
+// async function updateClue(id, answeredClue) {
+//   try {
+//     const rows = await connection.query(
+//       `UPDATE clues SET answered = ${answeredClue} WHERE clue_id= ${id}`
+//     );
+//     console.log("single clue updateClue" + JSON.stringify(answeredClue));
+//     return rows;
+//   } catch (err) {
+//     return err.stack;
+//   }
+// }
 
-async function setScore(gameid, score) {
-  try {
-    const rows = await connection.query(
-      `UPDATE games SET game_score = ${score} WHERE id=${gameid}`
-    );
-    return rows;
-  } catch (err) {
-    return err.stack;
-  }
-}
+// async function resetClues() {
+//   try {
+//     console.log("reset games");
+//     const rows = await connection.query(`UPDATE clues SET answered = null
+//     `);
+//     return rows;
+//   } catch (err) {
+//     return err.stack;
+//   }
+// }
 
-module.exports = {
-  getGameCategories,
-  getCategoryClues,
-  getGames,
-  getClue,
-  updateClue,
-  resetClues,
-  setScore,
-  getAllClues,
-};
+// async function setScore(gameid, score) {
+//   try {
+//     const rows = await connection.query(
+//       `UPDATE games SET game_score = ${score} WHERE id=${gameid}`
+//     );
+//     return rows;
+//   } catch (err) {
+//     return err.stack;
+//   }
+// }
+
+// module.exports = {
+//   getGameCategories,
+// getCategoryClues,
+// getGames,
+// getClue,
+// updateClue,
+// resetClues,
+// setScore,
+// getAllClues,
+// };
