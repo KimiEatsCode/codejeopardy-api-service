66259653b16335d2668df888c257cbc773db5a59
Kimi Rettig <sarahkimirettig@gmail.com>
2024-11-19 15:42:41 -0500

    x

--- index.js
+++ index.js
@@ -14,10 +14,7 @@ app.use(cors()); // Enable CORS for all routes
 
 // Optional: Configure specific CORS options
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://codejeopardyvue-s7pt.onrender.com"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
