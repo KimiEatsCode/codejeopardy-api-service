3d292808c8785d4a50d6121a72237154cef56484
Kimi Rettig <sarahkimirettig@gmail.com>
2024-11-19 15:55:04 -0500

    x

--- index.js
+++ index.js
@@ -10,14 +10,21 @@ const port = process.env.PORT;
 const gameMethods = require("./services/gameMethods");
 const gameMethodsRouter = require("./routes/gameRoutes");
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   // res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = ['http://localhost:3000', 'https://codejeopardyvue-s7pt.onrender.com/'];
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://codejeopardyvue-s7pt.onrender.com/",
+    "*",
+  ];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader("Access-Control-Allow-Origin", origin);
   }
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   res.header("Access-Control-Allow-credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
   next();
