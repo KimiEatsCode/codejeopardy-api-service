2a5cb7e5ce82f9a14829f6b63c62eb258f7e305a
Kimi Rettig <sarahkimirettig@gmail.com>
2024-11-21 14:00:11 -0500

    i had user instead of username

--- config.js
+++ config.js
@@ -4,18 +4,17 @@ const { Pool } = require('pg');
 
 
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
-// dotenv.config();
+// dotenv.config({ path: "./config.env" });
+dotenv.config();
 
 const pool = new Pool({
-  user: process.env.USERNAME,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
+  USERNAME: process.env.USERNAME,
+  HOST: process.env.HOST,
+  DATABASE: process.env.DATABASE,
+  PASSWORD: process.env.PASSWORD,
+  PORT: process.env.PORT,
   // ssl: process.env.SSL,
 });
-
 
 
 module.exports = pool;
