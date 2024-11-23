c933fab5bc923dec1090c78f8ebbc5cec303d243
Kimi Rettig <sarahkimirettig@gmail.com>
2024-11-23 18:07:12 -0500

    working methods

--- services/gameMethods.js
+++ services/gameMethods.js
@@ -1,115 +1,80 @@
 const db = require("../postgres-config");
-const client =require("../heroku-config-postgres");
+const client = require("../heroku-config-postgres");
 
-// async function getGames() {
 
-//  const rows =  client.query(`SELECT * FROM games`);
-//   return {
-//     rows,
-//   }
 
-// }
+const test = (req, res) => {
+  client.query("SELECT * FROM games", (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  });
+};
 
-const test =(req, res) => {
-   client.query('SELECT * FROM games', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-}
+const getGames = (req, res) => {
+  const rows = client.query("SELECT * FROM games");
+  return rows;
+};
 
-const getGames =(req, res) => {
-  try { const data = client.query('SELECT * FROM games');
-     return data;
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error fetching users');
-}
-}
-
-// async function getGames() {
-//   client.query("SELECT * FROM games", (err,result)=> {
-//   //   if(err) {
-//   //     throw err
-//   // }
-//   res.json(result.rows[0])
-// });
-// }
 
 async function getGameCategories() {
+  const rows = await client.query(`SELECT * FROM categories`);
 
-    const rows = await client.query(`SELECT * FROM categories`);
-
-    return {
-      rows,
-    }
-
+  return {
+    rows,
+  };
 }
 
-
 async function getAllClues() {
-
   const rows = await client.query(`SELECT * FROM clues`);
   return {
     rows,
-  }
+  };
 }
 
 async function getCategoryClues(catid) {
-
-    const rows = await client.query(
+  const rows = await client.query(
     `SELECT * FROM clues WHERE category_id = ${catid} ORDER BY value ASC`
   );
   return {
-    rows
-  }
-
+    rows,
+  };
 }
 ``;
 
-
 async function getClue(id) {
-
   const rows = await client.query(`SELECT * FROM clues WHERE clue_id = ${id}`);
   return {
     rows,
-}
-
-
+  };
 }
 
 async function updateClue(id, answeredClue) {
-
   const rows = await client.query(
     `UPDATE clues SET answered = ${answeredClue} WHERE clue_id= ${id}`
   );
   return {
     rows,
   };
-
 }
 
 async function resetClues() {
-
-    const rows = await client.query(`UPDATE clues SET answered = null
+  const rows = await client.query(`UPDATE clues SET answered = null
       `);
-      return {
-        rows,
-      };
-
+  return {
+    rows,
+  };
 }
 
 async function setScore(gameid, score) {
-
   const rows = await client.query(
     `UPDATE games SET game_score = ${score} WHERE id=${gameid}`
   );
   return {
     rows,
   };
-
 }
 
 module.exports = {
