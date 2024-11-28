edd64b02d52547245a1e06c4ab246598f890e3e3
Kimi Rettig <sarahkimirettig@gmail.com>
2024-11-27 20:50:20 -0500

    s

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(gameMethodsRouter.router8);
 app.use(cors());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://codejeo-frontend-0e4dcaeb89ec.herokuapp.com/*");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
