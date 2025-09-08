0257ae5dbf76b2ece8531543ff901db3074df629
Sarah Kimi Rettig <sarahkimirettig@gmail.com>
2025-09-08 13:18:14 -0400

    Deleted heroku config; added db config for env
    Returning to project no longer using heroku; using Vercel now

---
+++ db-config-postgres.js
@@ -0,0 +1,18 @@
+// require("dotenv").config({ path: "config.env" });
+import { config } from 'dotenv'; config();
+
+  import { Client } from 'pg'
+
+//Use connection string for production
+const client = new Client ({
+  connectionString: process.env.connectionString,
+  // user: process.env.LOCALUSER,
+  // host: process.env.LOCALHOST,
+  // database: process.env.LOCALDATABASE,
+  // password: process.env.LOCALPASSWORD,
+  // port: process.env.LOCALPORT,
+});
+
+client.connect();
+
+export { client };
